58598182cc9667c6f55406a9c2d9a4b9d0ad866b
Thiago Alessandro Batista <tbatista@weg.net>
2024-07-04 10:46:55 -0300

    feat: add dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkModeSwitch from './DarkModeSwitch'
 
 export default function Header() {
   return (
@@ -12,10 +13,14 @@ export default function Header() {
         <MenuItem title="about" address="about" Icon={BsFillInfoCircleFill} />
       </div>
 
+    <div className='flex items-center gap-4'>
+        <DarkModeSwitch />
       <Link href={"/"} className='flex gap-1 items-center'>
         <span className='text-2xl bg-amber-500 font-bold py-1 px-2 rounded-lg'>Movie</span>
         <span className='text-xl hidden sm:inline'>App</span>
       </Link>
+    </div>
+
     </div>
   )
 }
