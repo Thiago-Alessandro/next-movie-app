80b524fc556e1d0913f1979cc935ac562e2cb17d
Thiago Alessandro Batista <tbatista@weg.net>
2024-07-04 09:36:41 -0300

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+
+export default function Header() {
+  return (
+    <div>
+      <div className=''>
+        <MenuItem title="home" address="/" Icon />
+      </div>
+
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
