0a12703808f2338f78ecec7388f592f10c2f5aed
Thiago Alessandro Batista <tbatista@weg.net>
2024-07-04 10:55:40 -0300

    feat: creates the about page

---
+++ app/about/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600 '>
+        About
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue massa justo, id pulvinar mi sagittis in.
+         Vestibulum tristique tortor eget turpis euismod tempor sed sit amet neque. Maecenas elementum neque non ullamcorper sodales.
+         Aenean ullamcorper dictum nisl, quis varius lacus. Etiam eget velit vel augue egestas venenatis. Sed purus dui, aliquam eget tempor pulvinar, venenatis quis nisi.
+         Phasellus euismod sapien non feugiat imperdiet. Morbi at posuere orci.
+        </p>
+        
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dapibus odio a purus tempus pulvinar.
+         Phasellus eu justo sit amet turpis fermentum dignissim in quis ante. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+         Fusce consectetur feugiat rutrum. Nulla ac interdum urna. Aenean posuere elit a erat ultricies elementum. 
+         Vivamus vehicula, mi consequat convallis pretium, velit arcu ornare dui, id gravida nisi massa id metus. 
+         Vivamus leo leo, malesuada ac ipsum sit amet, egestas congue quam. Nunc leo nibh, pellentesque quis accumsan sed, tincidunt in tortor. 
+         Nam gravida ullamcorper urna at fermentum. Ut posuere mi non sem sollicitudin, id feugiat risus pretium. Sed vel eros pharetra, tempus lorem non, suscipit metus. 
+         Aliquam dapibus tortor quis nisl pellentesque euismod. Fusce malesuada quam sit amet enim commodo, ut varius leo lobortis. Sed vulputate erat vel bibendum elementum.
+        </p>
+    </div>
+  )
+}
